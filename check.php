d0f693f38ab74b8be57affc06b934204b11cf069
fromUSSR <62261546+fromUSSR@users.noreply.github.com>
2020-05-14 22:30:32 +0300

    Update check.php

--- check.php
+++ check.php
@@ -29,7 +29,7 @@ if (mb_strlen($pass)<5 || mb_strlen($pass) >30){
 }
 $pass = md5($pass."Rubic123");
 
-$mysql = new mysqli('http://rubilang.kikoriki.space/','rubilang','password','rubilang');
+$mysql = new mysqli('http://rubilang.kikoriki.space/','root','','rubilang');
 if ($mysql->connect_error != "") {
     die($mysql->connect_error);
 }

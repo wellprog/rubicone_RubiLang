ddb7352a2d546ec1a5b48f006e5bd481078120a2
Velor <velorkhalilov@gmail.com>
2020-05-17 14:00:10 +0300

    fix

--- auto.php
+++ auto.php
@@ -2,18 +2,17 @@
 
 session_start();
 
-$login =filter_var(trim ($_POST['login1']),
-FILTER_SANITIZE_STRING);
-$pass =filter_var(trim ($_POST['pass1']),
-FILTER_SANITIZE_STRING);
+$login =filter_var(trim ($_POST['login1']), FILTER_SANITIZE_STRING);
+$pass =filter_var(trim ($_POST['pass1']), FILTER_SANITIZE_STRING);
+
 $pass = md5($pass."Rubic123");
-$mysql = new mysqli('http://rubilang.kikoriki.space,'root','','rubilang');
+
+$mysql = new mysqli('localhost', 'rubilang', 'password','rubilang');
 $result = $mysql->query("SELECT * FROM `users` WHERE `login`='$login' AND `pass`='$pass'"); 
 $user = $result-> fetch_assoc();
 
 if($user == NULL){
-    echo "Такой пользователь не найден";
-    exit();   
+    die ("Такой пользователь не найден");
 }
 
 $_SESSION["user"] = $user;
